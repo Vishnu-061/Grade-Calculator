08cfe39be240000433a98e03fe255486cc0b1d0e
Vishnu Vardhan Reddy <119558422+Vishnu-061@users.noreply.github.com>
2025-08-14 09:29:20 +0530

    Update gradecalculator.java

--- gradecalculator.java
+++ gradecalculator.java
@@ -1,27 +1,4 @@
-
-/*import java.util.Scanner;
-public class gradecalculator {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter marks:");
-        int marks = sc.nextInt();
-        if(marks >95){
-            System.out.println("Grade A");
-        }else if(marks >=85 && marks <= 95){
-            System.out.println("Grade B");
-        }else if(marks >= 75 && marks<=85){
-            System.out.println("Grade c");
-        }else if(marks >=65 && marks <= 75){
-            System.out.println("Grade D");
-        }else if(marks >=55 && marks <=65){
-            System.out.println("Grade E");
-        }else{
-            System.out.println("Fail");
-        }
-    }
-    
-}*/
-
+//Garade calculator to used find the grade of the student
 import java.util.Scanner;
 public class gradecalculator{
     public static void main(String[] args){
@@ -45,3 +22,4 @@ public class gradecalculator{
 
 
 }
+
